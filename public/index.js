d971fd37cbce319099f1bc4197c07d3f204b1ce8
ilovell <ilovell@scottlogic.co.uk>
2016-08-16 16:05:50 +0100

    Added overlay

--- public/index.js
+++ public/index.js
@@ -1,14 +1,21 @@
+import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Loader from '../src/Loader'
 
 ReactDOM.render(
   <div>
-    <Loader size={40} />
-    <Loader>
+    <Loader size={40} text="Loading" loading />
+    <Loader size={20} text="Loading" color="#c0c" height={100} loading>
       <div>
         <p>Some content</p>
+        <p>Some content</p>
+        <p>Some content</p>
+        <p>Some content</p>
       </div>
+    </Loader>
+    <Loader loading overlay={false}>
+      <p>Some content</p>
     </Loader>
   </div>,
   document.getElementById('root')
--- src/Loader/Loader.jsx
+++ src/Loader/Loader.jsx
@@ -54,6 +54,54 @@ class Loader extends Component {
     clearTimeout(this.rotateTimer)
   }
 
+  getLoadingElement(size, thickness, color, text, textStyle) {
+    const radius = (size - thickness) / 2
+    const styles = getStyles(color, size, radius)
+
+    const textBaseStyle = {
+      margin: '0.2rem 0',
+      textAlign: 'center',
+    }
+
+    const rotationStyle = {
+      width: `${size}px`,
+      height: `${size}px`,
+      margin: '0 auto',
+      transformOrigin: 'center center',
+    }
+
+    if (this.animation.supported) {
+      rotationStyle[this.animation.animationString]
+        = `${this.keyframesId} 1s linear infinite`
+    } else {
+      const prefixes = 'ms Webkit'.split(' ')
+      for (const prefix of prefixes) {
+        rotationStyle[`${prefix}Transform`] = `rotateZ(${this.state.rotationDegrees}deg)`
+      }
+      rotationStyle.transform = `rotateZ(${this.state.rotationDegrees}deg)`
+    }
+
+    return (
+      <div>
+        <div style={rotationStyle}>
+          <svg style={styles.svg}>
+            <circle
+              ref="path"
+              style={styles.path}
+              cx={size / 2}
+              cy={size / 2}
+              r={radius}
+              fill="none"
+              strokeWidth={thickness}
+              strokeMiterlimit="20"
+            />
+          </svg>
+        </div>
+        {text ? <p style={Object.assign(textBaseStyle, textStyle)}>{text}</p> : null}
+      </div>
+    )
+  }
+
   ensureKeyframesAnimation(styleSheetId, keyframesId, keyframePrefix) {
     const keyframesSelector = `@${keyframePrefix}keyframes ${keyframesId}`
     const keyframesRules =
@@ -79,78 +127,84 @@ class Loader extends Component {
       thickness,
       text,
       textStyle,
+      loading,
+      children,
+      overlay,
+      overlayColor,
+      overlayOpacity,
+      height,
     } = this.props
 
-    const radius = (size - thickness) / 2
-    const styles = getStyles(color, size, radius)
-
-    const tableStyle = {
-      display: 'table-cell',
-      verticalAlign: 'middle',
-      textAlign: 'center',
+    if (!loading) {
+      return children || null
     }
 
-    const rotationStyle = {
-      width: `${size}px`,
-      height: `${size}px`,
-      margin: '0 auto',
-      transformOrigin: 'center center',
-    }
+    const loadingElement =
+      this.getLoadingElement(size, thickness, color, text, textStyle)
 
-    const textBaseStyle = {
-      margin: '0.2rem 0',
-    }
-
-    if (this.animation.supported) {
-      rotationStyle[this.animation.animationString]
-        = `${this.keyframesId} 1s linear infinite`
-    } else {
-      const prefixes = 'ms Webkit'.split(' ')
-      for (const prefix of prefixes) {
-        rotationStyle[`${prefix}Transform`] = `rotateZ(${this.state.rotationDegrees}deg)`
+    if (children) {
+      const stretchStyle = {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
       }
-      rotationStyle.transform = `rotateZ(${this.state.rotationDegrees}deg)`
+
+      const overlayStyle = {
+        backgroundColor: overlayColor,
+        opacity: overlayOpacity,
+        zIndex: 10,
+      }
+
+      const loadingElementStyle = {
+        display: 'table-cell',
+        verticalAlign: 'middle',
+        width: '100%',
+        height: '100%',
+      }
+
+      return (
+        <div style={{ position: 'relative' }}>
+          {overlay ? React.cloneElement(children, { style: { opacity: 0.3 } }) : null}
+          {overlay ? <div style={Object.assign({}, stretchStyle, overlayStyle)}></div> : null}
+          <div style={Object.assign({}, stretchStyle, { display: 'table', height, zIndex: 11 })}>
+            <div style={Object.assign({}, loadingElementStyle)}>
+              {loadingElement}
+            </div>
+          </div>
+        </div>
+      )
     }
 
-    return (
-      <div>
-      <div style={tableStyle}>
-        <div style={rotationStyle}>
-          <svg style={styles.svg}>
-            <circle
-              ref="path"
-              style={styles.path}
-              cx={size / 2}
-              cy={size / 2}
-              r={radius}
-              fill="none"
-              strokeWidth={thickness}
-              strokeMiterlimit="20"
-            />
-          </svg>
-        </div>
-        <p style={Object.assign(textBaseStyle, textStyle)}>{text}</p>
-      </div>
-    </div>
-    )
+    return loadingElement
   }
 }
 
 Loader.propTypes = {
+  height: PropTypes.number,
   color: PropTypes.string,
   size: PropTypes.number,
   thickness: PropTypes.number,
-  loading: PropTypes.boolean,
+  loading: PropTypes.bool,
   text: PropTypes.string,
   textStyle: PropTypes.object,
+  children: PropTypes.element,
+  overlay: PropTypes.bool,
+  overlayColor: PropTypes.string,
+  overlayOpacity: PropTypes.number,
 }
 
 Loader.defaultProps = {
+  height: 0,
   color: '#000',
   size: 20,
   thickness: 2,
   loading: false,
-  text: 'Loading',
+  text: null,
+  overlay: true,
+  overlayColor: '#000',
+  overlayOpacity: 0.5,
 }
 
 export default Loader
